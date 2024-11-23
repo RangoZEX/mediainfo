db3caa5f33a50ac161bf0ec43083bfdfec2e5644
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-11-23 13:04:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ logging.getLogger().setLevel(logging.ERROR)
  
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from plugins import Media
 from config import API_ID, API_HASH, BOT_TOKEN
 import pyromod.listen
  
