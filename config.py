030b1305f3f6b722b15d02da3f3913174352a406
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-11-23 12:45:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
-### config
+import os
+import logging
+from os import getenv, environ
+from dotenv import load_dotenv
+ 
+if os.path.exists('config.env'):
+    load_dotenv('config.env')
+    
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+ 
+class Config(object):
+    BOT_TOKEN = str(os.getenv('BOT_TOKEN', '1280510378:AAGY2MT44MjWKKUErTKIOfXPPKm33dntjkk'))
+    API_HASH = str(os.getenv('API_HASH', 'd02a52f4098d0d20b1be409818ec236f'))
+    API_ID = int(os.getenv('API_ID', '4216648'))
+    OWNER_ID = [int(x) for x in os.getenv("OWNER_ID", "1196263968").split()]
+ 
+globals().update({key: value for key, value in vars(Var).items() if not key.startswith('__')})
+
