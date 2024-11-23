1530c063f17c248c8d59754eea31546303f6b450
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-11-23 13:14:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,23 @@
 import logging
 import logging.config
 
-# Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from config import API_ID, API_HASH, BOT_TOKEN
-import pyromod.listen  # Ensure this is installed and configured
+import pyromod.listen
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
+            "FontsStyleBot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "Fonts"}  # Ensure "Fonts" directory exists
+            plugins={"root": "Fonts"}
         )
 
     async def start(self):
@@ -47,4 +46,3 @@ if __name__ == "__main__":
     except Exception as e:
         logging.error(f"Error running bot: {str(e)}")
         print(f"Error running bot: {str(e)}")
-     
