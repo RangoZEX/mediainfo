4804aca9667b8af245ac5003694d29626b12f882
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2024-11-25 19:56:24 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,41 @@
+import logging
+import asyncio
+import os
+from pyrogram import Client, filters
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+ 
+logging.basicConfig(level=logging.DEBUG)
+ 
+ 
+START_TEXT = """**Hey {},**\n
+ 
+**This is Mediainfo Bot
+"""
+ 
+@Client.on_callback_query(filters.regex("^start$"))                    
+@Client.on_message(filters.private & filters.command("start"))
+async def start(c, m):
+    last_name = f' {m.from_user.last_name}' if m.from_user.last_name else ''
+    mention = f"[{m.from_user.first_name}{last_name}](tg://user?id={m.from_user.id})"
+ 
+    if not getattr(m, 'data', None):
+        rango = await m.reply("**__Processing..__** ⏳", quote=True)
+    else:
+        rango = m.message
+    await rango.edit(
+         text=START_TEXT.format(mention),
+         reply_markup=InlineKeyboardMarkup(
+             [
+                 [
+                     InlineKeyboardButton('Close ❌', callback_data='cancel')
+                 ]
+             ]
+         ),
+         disable_web_page_preview=True,
+    )
+ 
+ 
